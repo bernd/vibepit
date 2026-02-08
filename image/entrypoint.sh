7d6c63517f8218d0aa699239794852f32d593095
Bernd Ahlers <bernd@bernd.wtf>
2026-02-08 12:48:02 +0100

    Install a global vibepit bashrc that we include in the user's bashrc

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -8,18 +8,6 @@ if [ ! -f "$HOME/.vibepit-initialized" ]; then
 	date > "$HOME/.vibepit-initialized"
 fi
 
-export HOME="/home/$CODE_USER"
-
-paths=()
-paths+=("$HOME/.local/node_modules/.bin")
-paths+=("$HOME/.deno/bin")
-paths+=("$HOME/.bun/bin")
-paths+=("$HOME/go/bin")
-
-for path in "${paths[@]}"; do
-	export PATH="$path:$PATH"
-done
-
 echo "+ Welcome to the pit!"
 
 exec /bin/bash --login
